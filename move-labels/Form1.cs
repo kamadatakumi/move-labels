7bed00d7022fa609af36a5ef1f081d414896fb57
kamadatakumi <yuuteayuu335dat@gmail.com>
2017-06-09 13:56:59 +0900

    演習8-2

--- move-labels/Form1.cs
+++ move-labels/Form1.cs
@@ -12,9 +12,16 @@ namespace move_labels
 {
     public partial class Form1 : Form
     {
+        int vx = -10, vy = -10;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left = vx;
+            label1.Top = vy;
         }
     }
 }
